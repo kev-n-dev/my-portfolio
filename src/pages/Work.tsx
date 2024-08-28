e0efae60303b3135c55ce7cd8c471d06cb1262f3
kevin <kevin@paay.co>
2024-08-28 15:12:09 -0400

    update resume

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,12 +1,19 @@
 // src/pages/Work.tsx
 import React from 'react';
+import PdfViewer from '../components/PdfViewer';
 
 const MySkills: React.FC = () => {
+  const pdfUrl = 'files/Profile.pdf'; // Replace with your PDF URL
+
+
   return (
-    <div>
-      <h1>Work</h1>
-     </div>
+    <div className='resume'>
+      <h1>Take a look at my resume here: 
+      </h1>
+      <PdfViewer pdfUrl={pdfUrl} />
+    </div>
   );
+
 };
 
 export default MySkills;
