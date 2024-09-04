ae611ef686d4f29ab436c13a1340bf24f1a6624a
kevin <kevin@paay.co>
2024-09-04 10:06:23 -0400

    adding new style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,29 +8,36 @@ import SocialLinks from './Socials';
 import { FaHouse } from 'react-icons/fa6';
 const Sidebar: React.FC = () => {
   return (
-    <div>
-      <div className='title'>
-        <ProfilePhoto
-          src="img/kevin.png"
-          alt="Kevin Christopher"
-          size={100}
-        />
-        <h2>Kevin Christopher</h2>
-        <div className='socialLinks'>        <SocialLinks></SocialLinks>
-        </div>
+    <div className='headerBar'>
+         <div className='ProfilePhoto'>
+            <ProfilePhoto
+              src="img/kevin.png"
+              alt="Kevin Christopher"
+              size={100}
+            />
+            <h2>Kevin Christopher</h2>
+         </div>
+        <div className='navigation'>
+          <nav>
+            <ul>
+              <li><Link to="/"> <FaHouse />Home </Link></li>
+              <li><Link to="/about"><FaUser /> About</Link></li>
+              <li><Link to="/skills"><FaTools /> MySkills</Link></li>
+              <li><Link to="/work"><FaBriefcase /> Resume</Link></li>
+              <li><Link to="/contact"><FaEnvelope /> Contact</Link></li>
+              <li><Link to="/blog"><FaBlog /> Blog</Link></li>
+            </ul>
+         </nav>
+           </div>
+
+
+      <div className='socialLinks'>        <SocialLinks></SocialLinks>
       </div>
-      <nav className='navigation'>
-        <ul>
-          <li><Link to="/"> <FaHouse />Home </Link></li>
-          <li><Link to="/about"><FaUser /> About</Link></li>
-          <li><Link to="/skills"><FaTools /> MySkills</Link></li>
-          <li><Link to="/work"><FaBriefcase /> Resume</Link></li>
-          <li><Link to="/contact"><FaEnvelope /> Contact</Link></li>
-          <li><Link to="/blog"><FaBlog /> Blog</Link></li>
-        </ul>
-      </nav>
+
+
     </div>
-  );
+    
+   );
 };
 
 export default Sidebar;
